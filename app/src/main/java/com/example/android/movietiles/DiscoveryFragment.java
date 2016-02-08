6fbbf7bdb871126a3e31382852918ae13cb1e421
Kevin Sebastian <kevin.sebastian@softventures.com.ph>
2016-02-08 23:48:30 +0800

    Added DiscoveryFragment
    Added the DiscoveryFragment with the fragment_discovery.xml layout.
    Attach this fragment to MainActivity.

---
+++ app/src/main/java/com/example/android/movietiles/DiscoveryFragment.java
@@ -0,0 +1,27 @@
+package com.example.android.movietiles;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DiscoveryFragment extends Fragment {
+
+    public DiscoveryFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_discovery, container, false);
+        return rootView;
+    }
+}
