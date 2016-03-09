fc9932ff23f3a53233f461850d9d79c6a7e1fda0
Kevin Sebastian <kevin.sebastian@softventures.com.ph>
2016-03-09 19:10:12 +0800

    Added the request to get movies
    Added a AsycTask to send a request to get the list of movies.
    Temporarily includes a log that displays the son response.

--- app/src/main/java/com/example/android/movietiles/DiscoveryFragment.java
+++ app/src/main/java/com/example/android/movietiles/DiscoveryFragment.java
@@ -1,10 +1,20 @@
 package com.example.android.movietiles;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class DiscoveryFragment extends Fragment {
 
@@ -23,5 +33,93 @@ public class DiscoveryFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_discovery, container, false);
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FetchMovieTask task = new FetchMovieTask();
+        task.execute();
+    }
+
+    public class FetchMovieTask extends AsyncTask<Void, Void, Void> {
+
+        private final String LOG_TAG = FetchMovieTask.class.getSimpleName();
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Log.v("JSON RESPONSE", sendGetMoviesRequest());
+            return null;
+        }
+
+        private String sendGetMoviesRequest() {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String jsonStr = null;
+
+            // URL parameters
+            String API_KEY = getString(R.string.api_key);
+            String SORT_BY = "popularity.desc";
+
+            try {
+                // Build the URL
+                Uri uri = Uri.parse("http://api.themoviedb.org/3/discover/movie")
+                        .buildUpon()
+                        .appendQueryParameter("api_key", API_KEY)
+                        .appendQueryParameter("sort_by", SORT_BY)
+                        .build();
+                URL url = new URL(uri.toString());
+
+                // Create the request, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty. No point in parsing.
+                    return null;
+                }
+                jsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the movie data, there's no point in attempting
+                // to parse it.
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("DiscoveryFragment", "Error closing stream", e);
+                    }
+                }
+            }
+            return jsonStr;
+        }
     }
 }
