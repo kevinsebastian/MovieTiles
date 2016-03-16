3a6b476f7b9954ac12a6e2e0c02ef04720321a08
Kevin Sebastian <kevin.sebastian@softventures.com.ph>
2016-03-16 13:06:09 +0800

    Display the passed MovieInfo fields in the DetailActivity fragment
    Change fragment_detail layout to use LinearLayout.
    
    Add TextViews to display the title, rating, release date and synopsis
    of the movie.

--- app/src/main/java/com/example/android/movietiles/DetailActivity.java
+++ app/src/main/java/com/example/android/movietiles/DetailActivity.java
@@ -1,11 +1,15 @@
 package com.example.android.movietiles;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.android.movietiles.model.MovieInfo;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -25,6 +29,19 @@ public class DetailActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+            Intent intent = getActivity().getIntent();
+            MovieInfo movieInfo = intent.getParcelableExtra("MOVIE_INFO");
+
+            TextView titleTextView = (TextView) rootView.findViewById(R.id.movie_title);
+            titleTextView.setText(movieInfo.getTitle());
+            TextView ratingTextView = (TextView) rootView.findViewById(R.id.movie_rating);
+            ratingTextView.setText(movieInfo.getRating());
+            TextView releaseDateTextView = (TextView) rootView.findViewById(R.id.movie_release_date);
+            releaseDateTextView.setText(movieInfo.getReleaseDate());
+            TextView synopsisTextView = (TextView) rootView.findViewById(R.id.movie_synopsis);
+            synopsisTextView.setText(movieInfo.getSynopsis());
+
             return rootView;
         }
     }
